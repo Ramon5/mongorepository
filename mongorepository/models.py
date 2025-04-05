90fffcb5b8a3a7a49843b4427caa7bf1d32b105a
Ramon Rodrigues <ramon.srodrigues01@gmail.com>
2025-04-04 22:41:10 -0300

    chore: add serialize

--- mongorepository/models.py
+++ mongorepository/models.py
@@ -1,7 +1,9 @@
 from abc import ABC
-from dataclasses import dataclass, field
+from dataclasses import asdict, dataclass, field, fields
 from datetime import datetime, timezone
 from typing import Any, Dict, Optional, Type
+
+from bson import ObjectId
 
 from mongorepository.utils.objects import PyObjectId
 
@@ -34,3 +36,51 @@ class MongoBaseModel(ABC):
             field_name = getattr(value, "alias", key)
             mapper[field_name] = 1
         return mapper
+    
+    @classmethod
+    def from_dict(cls, data: Dict[str, Any]) -> "MongoBaseModel":
+        init_kwargs = {}
+        for f in fields(cls):
+            field_name = f.name
+            dict_key = f.metadata.get("alias", field_name)
+
+            if dict_key not in data:
+                continue
+
+            value = data[dict_key]
+
+            if f.type == Optional[PyObjectId] and isinstance(value, str):
+                value = PyObjectId(value)
+
+            elif f.type == datetime and isinstance(value, str):
+                try:
+                    value = datetime.fromisoformat(value)
+                except ValueError:
+                    pass
+
+            init_kwargs[field_name] = value
+
+        return cls(**init_kwargs)
+
+    def serialize(self) -> Dict[str, Any]:
+        raw_dict = asdict(self)
+        doc = {}
+
+        for f in fields(self):
+            key = f.name
+            value = raw_dict[key]
+
+            dict_key = f.metadata.get("alias", key)
+
+            if isinstance(value, ObjectId):
+                value = str(value)
+
+            if isinstance(value, datetime):
+                value = value.isoformat()
+
+            doc[dict_key] = value
+
+        if doc.get("_id") is None:
+            del doc["_id"]
+
+        return doc
